b85a3bcc79933926a952477cdeeae5d777e7fe58
Ankan Pal <itsyourap@gmail.com>
2025-03-07 20:28:01 +0530

    Fix overall look of website

--- ems-frontend/src/components/HeaderComponent.jsx
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -1,15 +1,15 @@
-
-
 const HeaderComponent = () => {
   return (
     <div>
       <header>
-        <nav className='navbar navbar-dark bg-dark'>
-            <a className="navbar-brand" href="#https://www.javaguides.net">Employee Management System</a>
+        <nav className="navbar navbar-dark bg-dark px-3 py-2">
+          <a className="navbar-brand" href="/">
+            Employee Management System
+          </a>
         </nav>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,76 +1,89 @@
-import {useEffect, useState} from 'react'
-import { deleteEmployee, listEmployees } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { deleteEmployee, listEmployees } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 const ListEmployeeComponent = () => {
+  const [employees, setEmployees] = useState([]);
+  const navigator = useNavigate();
 
-    const [employees, setEmployees] =  useState([])
-    const navigator = useNavigate();
-    
-    useEffect(() =>{
-            getAllEmployees();
-            
-    }, [])
+  useEffect(() => {
+    getAllEmployees();
+  }, []);
 
-    function getAllEmployees(){
-        listEmployees().then((response) => {
-            setEmployees(response.data);
-        }).catch(error => {
-            console.error(error);  
-        });
-    }
+  function getAllEmployees() {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    function addNewEmployee(){
-            navigator('/add-employee');
-    }
+  function addNewEmployee() {
+    navigator("/add-employee");
+  }
 
-    function updateEmployee(id){
-        navigator( `/edit-employee/${id}`)
-    }
+  function updateEmployee(id) {
+    navigator(`/edit-employee/${id}`);
+  }
 
-    function removeEmployee(id){
-        console.log(id);
+  function removeEmployee(id) {
+    console.log(id);
 
-        deleteEmployee(id).then(() =>{
-                getAllEmployees()
-        }).catch(error => {
-            console.error(error);
-        })
-    }
+    deleteEmployee(id)
+      .then(() => {
+        getAllEmployees();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return (
-    <div className='container'>
-    <h2 className='text-center'>List of Employees</h2>
-    <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
-    <table className='table table-striped table-bordered'>
+    <div className="container py-4">
+      <h2 className="text-center">List of Employees</h2>
+      <button className="btn btn-primary mb-3" onClick={addNewEmployee}>
+        Add Employee
+      </button>
+      <table className="table table-striped table-bordered">
         <thead>
-            <tr>
-                <th>Employee Id</th>
-                <th>Employee First Name</th>                
-                <th>Employee Last Name</th>
-                <th>Employee Mail ID</th>
-                <th>Actions</th>
-            </tr>
+          <tr>
+            <th>Employee ID</th>
+            <th>Employee First Name</th>
+            <th>Employee Last Name</th>
+            <th>Employee Mail ID</th>
+            <th>Actions</th>
+          </tr>
         </thead>
         <tbody>
-            {
-                employees.map(employee =>
-                    <tr key={employee.id}>
-                    <td>{employee.id}</td>
-                    <td>{employee.firstName}</td>
-                    <td>{employee.lastName}</td>
-                    <td>{employee.email}</td>
-                    <td>
-                        <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
-                        <button className='btn btn-danger' onClick={() => removeEmployee(employee.id)}
-                            style={{marginLeft: '10px'}}>Delete</button>
-                    </td>
-                    </tr>)
-            }    
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.firstName}</td>
+              <td>{employee.lastName}</td>
+              <td>{employee.email}</td>
+              <td>
+                <button
+                  className="btn btn-info"
+                  onClick={() => updateEmployee(employee.id)}
+                >
+                  Update
+                </button>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => removeEmployee(employee.id)}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-    </table>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default ListEmployeeComponent
+export default ListEmployeeComponent;
