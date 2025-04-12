c10fddab8d64926c5bb199d1a7d785f40976404b
Ankan Pal <itsyourap@gmail.com>
2025-04-12 22:03:29 +0530

    feat: add HomeComponent and update FooterComponent with contributor links
    Introduce a new HomeComponent to serve as the landing page for the Employee Management System, replacing the previous default route to ListEmployeeComponent. Additionally, update the FooterComponent to include links to the project's GitHub repository and contributors, enhancing transparency and attribution.

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import EmployeeComponent from './components/EmployeeComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HomeComponent from './components/HomeComponent'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
     <BrowserRouter>
       <HeaderComponent />
       <Routes>
-        <Route path='/' element = {<ListEmployeeComponent />}></Route>
+        <Route path='/' element = {<HomeComponent />}></Route>
         <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
         <Route path='/add-employee'  element={<EmployeeComponent />}></Route>
         <Route path='/edit-employee/:id'  element={<EmployeeComponent />}></Route>
